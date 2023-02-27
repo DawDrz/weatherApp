378be31862a37d641be5c0edfcb833abeda8569f
Dawid Drzewiecki <ddr@mikrobit.pl>
2023-02-27 15:12:03 +0100

    Weather App

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    weatherApiBaseUrl:'https://openweatherapp.p.rapidapi.com/',
+    XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue: 'openweatherapp.p.rapidapi.com',
+    XRapidAPIKeyHeaderName:'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderValue:'ea03933c6dmsh89f52b296dbf3b7p1b7ed9jsnd922fb344eec'
+};
